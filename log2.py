384e9d9d2f46f22f0ccd51bb165aee2e75f00699
DGR22419 <75600994+dev22419@users.noreply.github.com>
2021-03-18 14:09:04 +0530

    Delete log2.py

--- log2.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-os.system('figlet -f big WELCOME | lolcat -S 27')
-os.system('figlet -f big T O | lolcat -S 27')
-os.system('figlet -f big TERMUX | lolcat -S 27')
-print("loading...")
-time.sleep(1)
-os.system('clear')
