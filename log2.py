271f20a082f806f64311fddd45c0341976096301
DGR22419 <75600994+dev22419@users.noreply.github.com>
2021-03-18 13:25:39 +0530

    Add files via upload

---
+++ log2.py
@@ -0,0 +1,9 @@
+import os
+import time
+
+os.system('figlet -f big WELCOME | lolcat -S 27')
+os.system('figlet -f big T O | lolcat -S 27')
+os.system('figlet -f big TERMUX | lolcat -S 27')
+print("loading...")
+time.sleep(1)
+os.system('clear')
