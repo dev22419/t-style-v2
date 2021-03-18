1641dc948253f756056eb6d5a0c851e9250123d1
DGR22419 <75600994+dev22419@users.noreply.github.com>
2021-03-18 13:21:54 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+apt install figlet
+
+apt install cowsay
+
+apt install python
+
+pip install lolcat
+
+chmod +x m.sh
+
+./m.sh
+
+cat file >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+mv t-style /data/data/com.termux/files/usr/share/
+
+rm -r /data/data/com.termux/files/home/t-style-v2
