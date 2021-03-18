c4a04ede0ba264a3b513b5ff743b7896756a08fe
DGR22419 <75600994+dev22419@users.noreply.github.com>
2021-03-18 14:09:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,4 @@ chmod +x m.sh
 
 cat file >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
-mkdir t-style /data/data/com.termux/files/usr/share/
-
-mv log2.py /data/data/com.termux/files/usr/share/t-style/
-
 rm -r /data/data/com.termux/files/home/t-style-v2
